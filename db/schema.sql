9ac381d34a698625d40d06587a54ca20670cbc19
Matthew O <mo@pop-os.localdomain>
2020-10-16 20:20:08 -0700

    setup tables with keys

--- db/schema.sql
+++ db/schema.sql
@@ -10,19 +10,24 @@ CREATE TABLE department(
     name VARCHAR(30) NOT NULL
 );
 
--- table for individual roles
+-- table for individual roles --
+--- dont want to cascade --- 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY department_id REFERENCES department(id)
 );
 
--- creates employee table
+-- creates employee table --
+--- do want to cascade ---
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     f_name VARCHAR(35) NOT NULL,
     l_name VARCHAR(35) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
